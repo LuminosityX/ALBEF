d821bc1d9d440651f7661ee9a8176efdd00658a6
liminosity <851148890@qq.com>
2023-02-25 18:39:55 +0800

    add_explanation

--- scheduler/scheduler_factory.py
+++ scheduler/scheduler_factory.py
@@ -6,11 +6,11 @@ from .tanh_lr import TanhLRScheduler
 from .step_lr import StepLRScheduler
 from .plateau_lr import PlateauLRScheduler
 
+# schedular: {sched: cosine, lr: 1e-4, epochs: 30, min_lr: 1e-5, decay_rate: 1, warmup_lr: 1e-5, warmup_epochs: 20, cooldown_epochs: 0}
+def create_scheduler(args, optimizer):                 
+    num_epochs = args.epochs                                       # 30
 
-def create_scheduler(args, optimizer):
-    num_epochs = args.epochs
-
-    if getattr(args, 'lr_noise', None) is not None:
+    if getattr(args, 'lr_noise', None) is not None:                # False
         lr_noise = getattr(args, 'lr_noise')
         if isinstance(lr_noise, (list, tuple)):
             noise_range = [n * num_epochs for n in lr_noise]
@@ -22,18 +22,18 @@ def create_scheduler(args, optimizer):
         noise_range = None
 
     lr_scheduler = None
-    if args.sched == 'cosine':
+    if args.sched == 'cosine':                                     # True
         lr_scheduler = CosineLRScheduler(
             optimizer,
-            t_initial=num_epochs,
-            t_mul=getattr(args, 'lr_cycle_mul', 1.),
-            lr_min=args.min_lr,
-            decay_rate=args.decay_rate,
-            warmup_lr_init=args.warmup_lr,
-            warmup_t=args.warmup_epochs,
-            cycle_limit=getattr(args, 'lr_cycle_limit', 1),
+            t_initial=num_epochs,                                  # 30 
+            t_mul=getattr(args, 'lr_cycle_mul', 1.),               # 1.
+            lr_min=args.min_lr,                                    # 1e-5
+            decay_rate=args.decay_rate,                            # 1
+            warmup_lr_init=args.warmup_lr,                         # 1e-5
+            warmup_t=args.warmup_epochs,                           # 20
+            cycle_limit=getattr(args, 'lr_cycle_limit', 1),        # 1
             t_in_epochs=True,
-            noise_range_t=noise_range,
+            noise_range_t=noise_range,                             # None
             noise_pct=getattr(args, 'lr_noise_pct', 0.67),
             noise_std=getattr(args, 'lr_noise_std', 1.),
             noise_seed=getattr(args, 'seed', 42),
