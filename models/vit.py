e836bdde47f42c1dde6f945912cef72739dcc298
liminosity <851148890@qq.com>
2023-02-27 09:55:27 +0800

    add_explanation_23_02_27

--- models/vit.py
+++ models/vit.py
@@ -11,12 +11,14 @@ from timm.models.layers import trunc_normal_, DropPath
 class Mlp(nn.Module):
     """ MLP as used in Vision Transformer, MLP-Mixer and related networks
     """
+    
+    # in_features=dim, hidden_features=mlp_hidden_dim, act_layer=act_layer, drop=drop           # 768, 768*4, nn.GELU, 0.
     def __init__(self, in_features, hidden_features=None, out_features=None, act_layer=nn.GELU, drop=0.):
         super().__init__()
-        out_features = out_features or in_features
-        hidden_features = hidden_features or in_features
+        out_features = out_features or in_features                                              # 768
+        hidden_features = hidden_features or in_features                                        # 768*4
         self.fc1 = nn.Linear(in_features, hidden_features)
-        self.act = act_layer()
+        self.act = act_layer()                                                                  # nn.GELU
         self.fc2 = nn.Linear(hidden_features, out_features)
         self.drop = nn.Dropout(drop)
 
@@ -30,12 +32,14 @@ class Mlp(nn.Module):
 
 
 class Attention(nn.Module):
+    
+    # dim, num_heads=num_heads, qkv_bias=qkv_bias, qk_scale=qk_scale, attn_drop=attn_drop, proj_drop=drop       # 768, 12, True, None, 0., 0.
     def __init__(self, dim, num_heads=8, qkv_bias=False, qk_scale=None, attn_drop=0., proj_drop=0.):
         super().__init__()
-        self.num_heads = num_heads
-        head_dim = dim // num_heads
+        self.num_heads = num_heads                                                                              # 12
+        head_dim = dim // num_heads                                                                             # 768 // 12  
         # NOTE scale factor was wrong in my original version, can set manually to be compat with prev weights
-        self.scale = qk_scale or head_dim ** -0.5
+        self.scale = qk_scale or head_dim ** -0.5                                                               
         self.qkv = nn.Linear(dim, dim * 3, bias=qkv_bias)
         self.attn_drop = nn.Dropout(attn_drop)
         self.proj = nn.Linear(dim, dim)
@@ -64,7 +68,7 @@ class Attention(nn.Module):
         attn = attn.softmax(dim=-1)
         attn = self.attn_drop(attn)
                 
-        if register_hook:
+        if register_hook:                                                                                         # Flase 
             self.save_attention_map(attn)
             attn.register_hook(self.save_attn_gradients)        
 
@@ -75,18 +79,21 @@ class Attention(nn.Module):
 
 
 class Block(nn.Module):
+    
+    # dim=embed_dim, num_heads=num_heads, mlp_ratio=mlp_ratio, qkv_bias=qkv_bias, qk_scale=qk_scale,              # 768, 12, 4., True, None
+    # drop=drop_rate, attn_drop=attn_drop_rate, drop_path=dpr[i], norm_layer=norm_layer                           # 0., 0., 0, partial(nn.LayerNorm, eps=1e-6)
 
     def __init__(self, dim, num_heads, mlp_ratio=4., qkv_bias=False, qk_scale=None, drop=0., attn_drop=0.,
                  drop_path=0., act_layer=nn.GELU, norm_layer=nn.LayerNorm):
         super().__init__()
-        self.norm1 = norm_layer(dim)
+        self.norm1 = norm_layer(dim)                                                                              # 768
         self.attn = Attention(
-            dim, num_heads=num_heads, qkv_bias=qkv_bias, qk_scale=qk_scale, attn_drop=attn_drop, proj_drop=drop)
+            dim, num_heads=num_heads, qkv_bias=qkv_bias, qk_scale=qk_scale, attn_drop=attn_drop, proj_drop=drop)  # 768, 12, True, None, 0., 0.
         # NOTE: drop path for stochastic depth, we shall see if this is better than dropout here
         self.drop_path = DropPath(drop_path) if drop_path > 0. else nn.Identity()
         self.norm2 = norm_layer(dim)
-        mlp_hidden_dim = int(dim * mlp_ratio)
-        self.mlp = Mlp(in_features=dim, hidden_features=mlp_hidden_dim, act_layer=act_layer, drop=drop)
+        mlp_hidden_dim = int(dim * mlp_ratio)                                                                     # 768*4
+        self.mlp = Mlp(in_features=dim, hidden_features=mlp_hidden_dim, act_layer=act_layer, drop=drop)           # 768, 768*4, nn.GELU, 0.
 
     def forward(self, x, register_hook=False):
         x = x + self.drop_path(self.attn(self.norm1(x), register_hook=register_hook))
@@ -99,6 +106,9 @@ class VisionTransformer(nn.Module):
     A PyTorch impl of : `An Image is Worth 16x16 Words: Transformers for Image Recognition at Scale`  -
         https://arxiv.org/abs/2010.11929
     """
+    
+    # img_size=config['image_res']=256, patch_size=16, embed_dim=768, depth=12, num_heads=12, 
+    # mlp_ratio=4, qkv_bias=True, norm_layer=partial(nn.LayerNorm, eps=1e-6)
     def __init__(self, img_size=224, patch_size=16, in_chans=3, num_classes=1000, embed_dim=768, depth=12,
                  num_heads=12, mlp_ratio=4., qkv_bias=True, qk_scale=None, representation_size=None,
                  drop_rate=0., attn_drop_rate=0., drop_path_rate=0., norm_layer=None):
@@ -121,26 +131,26 @@ class VisionTransformer(nn.Module):
             norm_layer: (nn.Module): normalization layer
         """
         super().__init__()
-        self.num_features = self.embed_dim = embed_dim  # num_features for consistency with other models
-        norm_layer = norm_layer or partial(nn.LayerNorm, eps=1e-6)
+        self.num_features = self.embed_dim = embed_dim  # num_features for consistency with other models    768
+        norm_layer = norm_layer or partial(nn.LayerNorm, eps=1e-6)                                         # partial(nn.LayerNorm, eps=1e-6)
 
-        self.patch_embed = PatchEmbed(
-            img_size=img_size, patch_size=patch_size, in_chans=in_chans, embed_dim=embed_dim)
+        self.patch_embed = PatchEmbed(                                                                     # from timm
+            img_size=img_size, patch_size=patch_size, in_chans=in_chans, embed_dim=embed_dim)              # 256 16 3 768
         num_patches = self.patch_embed.num_patches
 
-        self.cls_token = nn.Parameter(torch.zeros(1, 1, embed_dim))
-        self.pos_embed = nn.Parameter(torch.zeros(1, num_patches + 1, embed_dim))
-        self.pos_drop = nn.Dropout(p=drop_rate)
+        self.cls_token = nn.Parameter(torch.zeros(1, 1, embed_dim))                                        # [CLS]的可学习参数 
+        self.pos_embed = nn.Parameter(torch.zeros(1, num_patches + 1, embed_dim))                          # 位置编码
+        self.pos_drop = nn.Dropout(p=drop_rate)                                                            # 0
 
-        dpr = [x.item() for x in torch.linspace(0, drop_path_rate, depth)]  # stochastic depth decay rule
+        dpr = [x.item() for x in torch.linspace(0, drop_path_rate, depth)]  # stochastic depth decay rule  全是0吧
         self.blocks = nn.ModuleList([
             Block(
-                dim=embed_dim, num_heads=num_heads, mlp_ratio=mlp_ratio, qkv_bias=qkv_bias, qk_scale=qk_scale,
-                drop=drop_rate, attn_drop=attn_drop_rate, drop_path=dpr[i], norm_layer=norm_layer)
+                dim=embed_dim, num_heads=num_heads, mlp_ratio=mlp_ratio, qkv_bias=qkv_bias, qk_scale=qk_scale,      # 768, 12, 4., True, None
+                drop=drop_rate, attn_drop=attn_drop_rate, drop_path=dpr[i], norm_layer=norm_layer)                  # 0., 0., 0, partial(nn.LayerNorm, eps=1e-6)
             for i in range(depth)])
         self.norm = norm_layer(embed_dim)
 
-        trunc_normal_(self.pos_embed, std=.02)
+        trunc_normal_(self.pos_embed, std=.02)                                                             # ??
         trunc_normal_(self.cls_token, std=.02)
         self.apply(self._init_weights)
 
@@ -159,40 +169,40 @@ class VisionTransformer(nn.Module):
 
     def forward(self, x, register_blk=-1):
         B = x.shape[0]
-        x = self.patch_embed(x)
+        x = self.patch_embed(x)                        # [b, patch_num, dim]
 
         cls_tokens = self.cls_token.expand(B, -1, -1)  # stole cls_tokens impl from Phil Wang, thanks
-        x = torch.cat((cls_tokens, x), dim=1)
+        x = torch.cat((cls_tokens, x), dim=1)          # 将[CLS]添加了上去
   
-        x = x + self.pos_embed[:,:x.size(1),:]
+        x = x + self.pos_embed[:,:x.size(1),:]         # sum了位置编码
         x = self.pos_drop(x)
 
         for i,blk in enumerate(self.blocks):
-            x = blk(x, register_blk==i)
+            x = blk(x, register_blk==i)                # register_blk==i 一直是False
         x = self.norm(x)
         
-        return x
+        return x                                       # [b, patch_num, dim]
 
 
 
-def interpolate_pos_embed(pos_embed_checkpoint, visual_encoder):        
+def interpolate_pos_embed(pos_embed_checkpoint, visual_encoder):                                         # state_dict['pos_embed'], self.visual_encoder
     # interpolate position embedding
-    embedding_size = pos_embed_checkpoint.shape[-1]
-    num_patches = visual_encoder.patch_embed.num_patches
-    num_extra_tokens = visual_encoder.pos_embed.shape[-2] - num_patches
+    embedding_size = pos_embed_checkpoint.shape[-1]                                                      # 应该是768
+    num_patches = visual_encoder.patch_embed.num_patches                                                 
+    num_extra_tokens = visual_encoder.pos_embed.shape[-2] - num_patches                                  # 应该就一个[CLS]，所以为1
     # height (== width) for the checkpoint position embedding
-    orig_size = int((pos_embed_checkpoint.shape[-2] - num_extra_tokens) ** 0.5)
+    orig_size = int((pos_embed_checkpoint.shape[-2] - num_extra_tokens) ** 0.5)                          # 根号下的表示 ??? 什么意思啊
     # height (== width) for the new position embedding
     new_size = int(num_patches ** 0.5)
 
-    if orig_size!=new_size:
+    if orig_size!=new_size:                                                                              # 所加载的权重中的位置编码与所使用模型间的位置编码长度不一致时，需要进行插值
         # class_token and dist_token are kept unchanged
         extra_tokens = pos_embed_checkpoint[:, :num_extra_tokens]
         # only the position tokens are interpolated
         pos_tokens = pos_embed_checkpoint[:, num_extra_tokens:]
-        pos_tokens = pos_tokens.reshape(-1, orig_size, orig_size, embedding_size).permute(0, 3, 1, 2)
+        pos_tokens = pos_tokens.reshape(-1, orig_size, orig_size, embedding_size).permute(0, 3, 1, 2)    # embedding_size提到第二位
         pos_tokens = torch.nn.functional.interpolate(
-            pos_tokens, size=(new_size, new_size), mode='bicubic', align_corners=False)
+            pos_tokens, size=(new_size, new_size), mode='bicubic', align_corners=False)                  # torch的插值函数
         pos_tokens = pos_tokens.permute(0, 2, 3, 1).flatten(1, 2)
         new_pos_embed = torch.cat((extra_tokens, pos_tokens), dim=1)
         print('reshape position embedding from %d to %d'%(orig_size ** 2,new_size ** 2))
